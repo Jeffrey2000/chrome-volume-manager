c603a74ef35041b0143b788f6dee8aa6502b53bd
Jeffrey2000 <jeffreykola15@gmail.com>
2020-07-31 18:37:13 +0100

    added feature of showing all tabs on the popup

--- src/interfaces/Message.ts
+++ src/interfaces/Message.ts
@@ -1,3 +1,6 @@
+
+//This is basically a getter and setter for the tabs
+//Communicated via message
 type Message = {
   name: 'get-tab-volume',
   tabId: number
--- src/popup.ts
+++ src/popup.ts
@@ -4,9 +4,18 @@ import 'chrome-extension-async'
 import Message from './interfaces/Message'
 import { MDCSlider } from '@material/slider'
 
+
+//var titles:Array <String> = [];
+
 const sliderElem: HTMLDivElement = document.querySelector('#volume-slider')
 const slider = new MDCSlider(sliderElem)
-
+const openTabsSection : HTMLDivElement = document.querySelector('#all_tabs')
+window.onload = function(){
+  
+  //  chrome.runtime.getBackgroundPage.console.log("here");
+     getAllTabNames();
+  }
+  
 void (async () => {
   // Hide the slider until we know the initial volume
   sliderElem.style.opacity = '0'
@@ -22,11 +31,15 @@ slider.listen('MDCSlider:input', () => {
   setActiveTabVolume(value)
 })
 
+//Change to mondaic functions with the argument of the tab object
+//rename to getTabVolume
 async function getActiveTabVolume () {
   const tabId = await getActiveTabId()
+  
   const message: Message = { name: 'get-tab-volume', tabId }
   return chrome.runtime.sendMessage(message)
 }
+
 
 async function setActiveTabVolume (value: number) {
   const tabId = await getActiveTabId()
@@ -35,6 +48,26 @@ async function setActiveTabVolume (value: number) {
 }
 
 async function getActiveTabId () {
+  //The logic for identifying an active tab needs to change
   const [activeTab] = await chrome.tabs.query({ active: true, currentWindow: true })
   return activeTab.id
 }
+
+
+
+async function getAllTabNames(){
+  chrome.runtime.onMessage.addListener(async (req, sender, response)=>{
+    req = JSON.parse(req);
+    req.forEach(id => {
+      chrome.tabs.get(parseInt(id),(tab)=>{
+       openTabsSection.innerHTML+=`<br/> <a target="_blank" href=${tab.url}>${tab.title}</a>`
+     });
+   });
+
+  })
+
+
+}
+
+
+
